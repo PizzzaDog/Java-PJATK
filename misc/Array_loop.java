9e70ed104bdebd63a930995f48750e700dd7dbf6
Alice Lashuk <alice.lashuk@gmail.com>
2018-10-30 10:33:42 +0100

    First commit

---
+++ misc/Array_loop.java
@@ -0,0 +1,9 @@
+public class Array_loop{
+    public static void main(String[] args){
+        int [] arr = {1,2,3};
+        int i;
+        for (i=0; i< 3; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
