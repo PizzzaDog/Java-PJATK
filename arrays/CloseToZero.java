a3cfe9c2670d664682a4be7eba8e17e97fa29595
Alice Lashuk <alice.lashuk@gmail.com>
2018-11-11 11:47:03 +0300

    First commit

---
+++ arrays/CloseToZero.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class CloseToZero{
+    public static void main(String[] args){
+
+        Random rnd = new Random();
+         int ranVal2 = rnd.nextInt(20);
+         int[] arr = new int[ranVal2];
+
+       for (int i = 0; i < arr.length; i++) { //put random values
+            int ranVal1 = rnd.nextInt(20);
+            arr[i] = Math.abs(ranVal1);
+        }
+
+        for (int k = 0; k < arr.length; k++) { // print array
+            System.out.print(arr[k]+" ");
+        }
+        System.out.println();
+
+        int min1 = arr[0];
+        for (int h=0; h<arr.length; h++){  //find min1
+            if(arr[h]<=min1){
+                min1 = arr[h];
+            }
+        }
+         System.out.println(min1);
+
+         int min2 = arr[0];
+
+        for (int h=0; h<arr.length; h++){  //find min2
+            if (arr[h] > min1) {
+                min2 = (min2 > arr[h])? arr[h]:min2;
+                }
+            }
+
+        System.out.println(min2);
+        System.out.println(min1+min2);
+        }
+
+
+
+    }
---
+++ arrays/OddEven.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class OddEven {
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+        int ranVal1 = rnd.nextInt(20);
+        int[] arr = new int[ranVal1];
+        for (int i = 0; i < arr.length; i++) {
+            int ranVal2 = rnd.nextInt();
+            arr[i] = Math.abs(ranVal2);
+        }
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }
+        System.out.println();
+
+        int odd = 0;
+        int l = 0;
+        int even = 0;
+        int f = 0;
+        for (int r = 0; r < arr.length; r++) {
+            if (arr[r] % 2 == 1) {
+                odd = arr[r];
+                l = r;
+               break;
+            }
+        }
+        for (int w = 0; w < arr.length; w++) {
+            if (arr[w] % 2 == 0) {
+                even = arr[w];
+                f = w;
+            }
+        }
+        if (even == 0) {
+            System.out.println("They are all odd");
+        } else if (odd == 0) {
+            System.out.println("They are all even");
+        } else {
+            int temp = arr[l];
+            arr[l] = arr[f];
+            arr[f] = temp;
+            for (int k = 0; k < arr.length; k++) {
+                System.out.println(arr[k]);
+            }
+         }
+     }
+}
