917d9cfbd0f58548fa5db91edae92b8f487096cb
alice-lashuk <alice.lashuk@gmail.com>
2019-04-02 16:30:30 +0200

    completed

--- GUI/lecture/task_set_4/Task_1.java
+++ GUI/lecture/task_set_4/Task_1.java
@@ -1,7 +1,12 @@
+import java.util.TreeSet;
+
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Collection;
 import java.util.ArrayList;
-class Person{
+import java.util.TreeSet;
+class Person implements Comparable<Person>{
     private String name;
     private int yearOfBirth;
     public Person(String n, int y) {
@@ -12,19 +17,42 @@ class Person{
         Person tmp = new Person(name, year);
         return coll.contains(tmp);
     }
+
+    //to compare in TreeMap
+    @Override
+    public int compareTo(Person f) {
+        boolean n = name.equals(f.name);
+        int diff = yearOfBirth - f.yearOfBirth;
+
+        if ((diff < 0) || !n) {
+            return -1;
+        }
+        else if ((diff > 0)|| !n){
+            return +1;
+        } 
+        else return 0;
+    }
+
     @Override 
     public String toString(){
         return "name: " + name + " yearOfBirth: " + yearOfBirth;
     }
 
-   @Override 
-   public boolean equals(Object other){
+    // to compare different objects with the same instances in HashSet
+    @Override
+    public int hashCode(){
+        return name.hashCode() + new Integer(yearOfBirth).hashCode();
+    } 
+
+    // to compare different objects with the same instances
+    @Override 
+    public boolean equals(Object other){
        if(other == null || getClass()!= other.getClass()) {
            return false;
        }
        Person p = (Person)other;
         return yearOfBirth==p.yearOfBirth && name.equals(p.name);
-       }
+    }
 }
 
 public class Task_1 {
@@ -35,6 +63,9 @@ public class Task_1 {
         String tmp;
         int count = 0;
         ArrayList<Person> list= new ArrayList<Person>();
+
+        //read data
+
         while(!name.equals("end")){
             //TODO: ecxeption
             tmp = scan.next();
@@ -54,12 +85,36 @@ public class Task_1 {
             list.add(new Person(name, y));
             name = scan.next();
         }
-        
+
+        //print list
         for (Person a : list) {
             System.out.println(a);
         }
 
+        
         System.out.println(Person.findInColl(list, "alice", 2000));
+
+        //create hashSet
+        Set<Person> hSet = new HashSet<>(list);
+
+        System.out.println();
+
+        //print hashSet
+        for (Person b : hSet) {
+            System.out.println(b);
+        }
+
+        System.out.println(Person.findInColl(hSet, "alice", 2000));
+
+        //create TreeSet
+        Set<Person> trSet = new TreeSet<>(list);
+
+        for (Person f : list) {
+            System.out.println(f);
+        }
+
+        System.out.println(Person.findInColl(trSet, "alice", 2000));
+
     }
 
 }
